2e302e054c80bcb7cca502bc06559d3aba1636a1
Ferdinand Swoboda <ferdinand.swoboda@googlemail.com>
2017-11-11 23:05:18 +1100

    refactor transformation with Java 8 lambda expressions and indexes

--- src/main/java/transformation/WorksToHTMLTransformation.java
+++ src/main/java/transformation/WorksToHTMLTransformation.java
@@ -9,9 +9,10 @@ import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 import java.io.StringWriter;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class WorksToHTMLTransformation implements Transformation<List<Work>, Map<String, StringWriter>> {
 
@@ -29,108 +30,63 @@ public class WorksToHTMLTransformation implements Transformation<List<Work>, Map
 
     @Override
     public Map<String, StringWriter> apply(List<Work> works) {
-        // first image urls
-        List<String> indexImageUrls = new LinkedList<>();
 
-        // first image urls per camera make
-        Map<String, List<String>> imageUrlsPerCameraMake = new HashMap<>();
+        //first, construct an index on the list of works to enable fast search by camera make
+        Map<String, List<Work>> makeIndex = works.stream().collect(Collectors.groupingBy(Work::getExif_make));
 
-        // first image urls per camera make and model
-        Map<String, Map<String, List<String>>> imageUrlsPerCameraMakeAndModel = new HashMap<>();
+        //second, construct a 2-layered index to enable fast search by camera make and camera model
+        Map<String, Map<String, List<Work>>> makeAndModelIndex = works.stream().collect(Collectors.groupingBy(Work::getExif_make, Collectors.groupingBy(Work::getExif_model)));
 
-        for(Work work : works) {
-            if(indexImageUrls.size() < 10) {
-                indexImageUrls.add(work.getUrlsOfImages().get(2));
-            }
-
-            if(!imageUrlsPerCameraMake.containsKey(work.getExif_make())) {
-                imageUrlsPerCameraMake.put(work.getExif_make(), new LinkedList<>());
-                imageUrlsPerCameraMakeAndModel.put(work.getExif_make(), new HashMap<>());
-            }
-            if(imageUrlsPerCameraMake.get(work.getExif_make()).size() < 10) {
-                imageUrlsPerCameraMake.get(work.getExif_make()).add(work.getUrlsOfImages().get(2));
-            }
-
-            if(!imageUrlsPerCameraMakeAndModel.get(work.getExif_make()).containsKey(work.getExif_model())) {
-                imageUrlsPerCameraMakeAndModel.get(work.getExif_make()).put(work.getExif_model(), new LinkedList<>());
-            }
-            if(imageUrlsPerCameraMakeAndModel.get(work.getExif_make()).get(work.getExif_model()).size() < 10) {
-                imageUrlsPerCameraMakeAndModel.get(work.getExif_make()).get(work.getExif_model()).add(work.getUrlsOfImages().get(2));
-            }
-        }
-
-        return buildHTMLPages(indexImageUrls, imageUrlsPerCameraMake, imageUrlsPerCameraMakeAndModel);
+        return buildHTMLPages(works, makeIndex, makeAndModelIndex);
     }
 
-    private Map<String, StringWriter> buildHTMLPages(List<String> indexImageUrls, Map<String, List<String>> cameraMakeToImageUrls, Map<String, Map<String, List<String>>> cameraMakeAndModelToImageUrls) {
+    private Map<String, StringWriter> buildHTMLPages(List<Work> works, Map<String, List<Work>> makeIndex, Map<String, Map<String, List<Work>>> makeAndModelIndex) {
         String indexDocumentName = getDocumentNameForIndexHTMLPage();
-        Map<String, String> cameraMakeToDocumentName = new HashMap<>();
-        Map<String, Map<String, String>> cameraMakeAndModelToDocumentName = new HashMap<>();
-
-        // first, initialise the mappings from images by a camera make (e.g. "SUPERCAM") to associated document name (e.g. imagesOf_SUPERCAM.html)
-        for(Map.Entry<String, List<String>> imageUrlsOfCameraMake : cameraMakeToImageUrls.entrySet()) {
-            String cameraMake = imageUrlsOfCameraMake.getKey();
-            cameraMakeToDocumentName.put(cameraMake, getDocumentNameForCameraMakeHTMLPage(cameraMake));
-            cameraMakeAndModelToDocumentName.put(cameraMake, new HashMap<>());
-
-            // second, initialise the mappings from images by a camera model (e.g. "S3000") to associated document name (e.g. imagesOf_SUPERCAM_S3000.html)
-            for(Map.Entry<String, List<String>> imageUrlsOfCameraMakeAndModel : cameraMakeAndModelToImageUrls.get(cameraMake).entrySet()) {
-                String cameraModel = imageUrlsOfCameraMakeAndModel.getKey();
-                cameraMakeAndModelToDocumentName.get(cameraMake).put(cameraModel, getDocumentNameForCameraMakeAndModelHTMLPage(cameraMake, cameraModel));
-            }
-        }
-
+        Map<String, String> cameraMakeToDocumentName = makeIndex.keySet().stream().collect(Collectors.toMap(Function.identity(), cameraMake -> getDocumentNameForCameraMakeHTMLPage(cameraMake)));
+        Map<String, Map<String, String>> cameraMakeAndModelToDocumentName = makeAndModelIndex.entrySet().stream().collect(Collectors.toMap(entry -> entry.getKey(), entry -> entry.getValue().keySet().stream().collect(Collectors.toMap(Function.identity(), cameraModel -> getDocumentNameForCameraMakeAndModelHTMLPage(entry.getKey(), cameraModel)))));
 
         // then create the html pages and map the document name to the corresponding html page
+        StringWriter htmlPage = null;
         Map<String, StringWriter> documentNameToHTMLPage = new HashMap<>();
 
-        // create the index page and map its document name to it
-        documentNameToHTMLPage.put(indexDocumentName, buildIndexHTMLPage(cameraMakeToDocumentName, indexImageUrls));
+        // create the index html page and map its document name to it
+        htmlPage = buildIndexHTMLPage(cameraMakeToDocumentName, works.subList(0, 10));
+        documentNameToHTMLPage.put(indexDocumentName, htmlPage);
 
-        // for each camera make,
-        for(Map.Entry<String, String> cameraMake : cameraMakeToDocumentName.entrySet()) {
-            // create a page
-            StringWriter htmlPage = buildCameraMakeHTMLPage(indexDocumentName, cameraMakeAndModelToDocumentName.get(cameraMake.getKey()), cameraMake.getKey(), cameraMakeToImageUrls.get(cameraMake));
-            documentNameToHTMLPage.put(cameraMake.getKey(), htmlPage);
+        // for each camera make, map its document name to the created HTML page
+        makeIndex.forEach((cameraMake, worksOfCameraMake) -> documentNameToHTMLPage.put(cameraMakeToDocumentName.get(cameraMake), buildCameraMakeHTMLPage(indexDocumentName, cameraMakeAndModelToDocumentName.get(cameraMake), worksOfCameraMake.subList(0, 10))));
 
-            // and create a page for each camera model of that camera make
-            for(Map.Entry<String, String> cameraModel : cameraMakeAndModelToDocumentName.get(cameraMake.getKey()).entrySet()) {
-                htmlPage = buildCameraMakeAndModelHTMLPage(indexDocumentName, cameraMakeToDocumentName.get(cameraMake), cameraMake.getKey(), cameraModel.getKey(), cameraMakeAndModelToImageUrls.get(cameraMake).get(cameraModel));
-                documentNameToHTMLPage.put(cameraModel.getKey(), htmlPage);
-            }
-        }
+        // create a html page for each camera make's model and map its document name to it
+        makeAndModelIndex.forEach((cameraMake, cameraModelToWorks) -> cameraModelToWorks.forEach((cameraModel, worksOfCameraModel) -> documentNameToHTMLPage.put(cameraMakeAndModelToDocumentName.get(cameraMake).get(cameraModel), buildCameraMakeAndModelHTMLPage(indexDocumentName, cameraMakeToDocumentName.get(cameraMake), worksOfCameraModel.subList(0, 10)))));
 
         return documentNameToHTMLPage;
     }
 
-    private StringWriter buildIndexHTMLPage( Map<String, String> cameraMakeToDocumentName, List<String> imageUrls) {
+    private StringWriter buildIndexHTMLPage( Map<String, String> cameraMakeToDocumentName, List<Work> works) {
         StringWriter htmlDocument = new StringWriter();
         Context context = new Context();
         context.setVariable("cameraMakePageLinks", cameraMakeToDocumentName);
-        context.setVariable("imageUrls", imageUrls);
+        context.setVariable("works", works);
         templateEngine.process("index", context, htmlDocument);
         return htmlDocument;
     }
 
-    private StringWriter buildCameraMakeHTMLPage(String indexDocumentName, Map<String, String> cameraModelToDocumentName, String cameraMake, List<String> imageUrls) {
+    private StringWriter buildCameraMakeHTMLPage(String indexDocumentName, Map<String, String> cameraModelToDocumentName, List<Work> works) {
         StringWriter htmlDocument = new StringWriter();
         Context context = new Context();
         context.setVariable("indexPageLink", indexDocumentName);
         context.setVariable("cameraModelPageLinks", cameraModelToDocumentName);
-        context.setVariable("imageUrls", imageUrls);
-        context.setVariable("currentMake", cameraMake);
+        context.setVariable("works", works);
         templateEngine.process("imagesByCameraMake", context, htmlDocument);
         return htmlDocument;
     }
 
-    private StringWriter buildCameraMakeAndModelHTMLPage(String indexDocumentName, String cameraMakeDocumentName, String cameraMake, String cameraModel, List<String> imageUrls) {
+    private StringWriter buildCameraMakeAndModelHTMLPage(String indexDocumentName, String cameraMakeDocumentName, List<Work> works) {
         StringWriter htmlDocument = new StringWriter();
         Context context = new Context();
         context.setVariable("indexPageLink", indexDocumentName);
         context.setVariable("cameraMakePageLink", cameraMakeDocumentName);
-        context.setVariable("imageUrls", imageUrls);
-        context.setVariable("currentMake", cameraMake);
-        context.setVariable("currentModel", cameraModel);
+        context.setVariable("works", works);
         templateEngine.process("imagesByCameraMakeAndModel", context, htmlDocument);
         return htmlDocument;
     }
