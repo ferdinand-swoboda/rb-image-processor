9ea7446fb00e5d283ae65bfe67d6ebdee597e30c
Ferdinand Swoboda <ferdinand.swoboda@googlemail.com>
2017-11-12 12:10:23 +1100

    Make parsing certain XML elements optional

--- src/main/java/domain/Work.java
+++ src/main/java/domain/Work.java
@@ -16,13 +16,20 @@ public class Work {
     @Path("urls")
     private List<String> urlsOfImages;
 
-    @Element(name = "model")
+    @Element(name = "model", required = false)
     @Path("exif")
     private String exif_model;
 
-    @Element(name = "make")
+    @Element(name = "make", required = false)
     @Path("exif")
     private String exif_make;
+
+    public Work(int id, List<String> urlsOfImages, String exif_model, String exif_make) {
+        this.id = id;
+        this.urlsOfImages = urlsOfImages;
+        this.exif_model = exif_model;
+        this.exif_make = exif_make;
+    }
 
     public int getId() {
         return id;
@@ -54,5 +61,27 @@ public class Work {
 
     public void setExif_make(String exif_make) {
         this.exif_make = exif_make;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Work work = (Work) o;
+
+        if (id != work.id) return false;
+        if (urlsOfImages != null ? !urlsOfImages.equals(work.urlsOfImages) : work.urlsOfImages != null) return false;
+        if (exif_model != null ? !exif_model.equals(work.exif_model) : work.exif_model != null) return false;
+        return exif_make != null ? exif_make.equals(work.exif_make) : work.exif_make == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (urlsOfImages != null ? urlsOfImages.hashCode() : 0);
+        result = 31 * result + (exif_model != null ? exif_model.hashCode() : 0);
+        result = 31 * result + (exif_make != null ? exif_make.hashCode() : 0);
+        return result;
     }
 }
