17662a714554d3a29229d3f0492646a0e46911db
Ferdinand Swoboda <ferdinand.swoboda@googlemail.com>
2017-11-12 12:12:20 +1100

    Add image processor test

---
+++ src/test/java/ImageProcessorTest.java
@@ -0,0 +1,23 @@
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class ImageProcessorTest {
+
+    @Test
+    public void processImagesShouldResultIn2CanonModelPages() throws IOException{
+        String[] args = new String[2];
+        args[0] = "http://take-home-test.herokuapp.com/api/v1/works.xml";
+        Path outputDir = Files.createTempDirectory("rb-image-processor-output", null);
+        args[1] = outputDir.toString();
+        ImageProcessor.main(args);
+
+        Assert.assertTrue(new File(outputDir.toFile(), "imagesOf_Canon_Canon-EOS-20D.html").exists());
+        Assert.assertTrue(new File(outputDir.toFile(), "imagesOf_Canon_Canon-EOS-400D-DIGITAL.html").exists());
+    }
+}
